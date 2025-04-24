7448de631367a34d09b0e3f236efbaf6ba0ba01c
Raissa Nunes <raissan084@gmail.com>
2025-04-24 10:36:00 -0300

    resolução exercicios

--- listas/lista01.pl
+++ listas/lista01.pl
@@ -65,3 +65,96 @@ soma_e_conta([H|T], Soma, Contador) :-
     soma_e_conta(T, Soma1, Contador1),
     Soma is Soma1 + H,
     Contador is Contador1 + 1.
+
+
+% Exercicio 10
+% Caso especial: lista vazia
+minimo([], _) :-
+    write('Erro: lista vazia, não há mínimo.'), nl,
+    fail.
+
+% Caso base: um único elemento
+minimo([X], X).
+
+% Caso recursivo
+minimo([H|T], Min) :- minimo(T, MinT), ( H < MinT -> Min = H ; Min = MinT ).
+
+
+% Exercicio 11
+% Caso base: limite inferior maior que o superior → lista vazia
+intervalo(Inf, Sup, []) :- Inf > Sup.
+
+% Caso recursivo: adiciona o número atual e avança
+intervalo(Inf, Sup, [Inf|Resto]) :-
+    Inf =< Sup,
+    Prox is Inf + 1,
+    intervalo(Prox, Sup, Resto).
+
+
+
+% Exercicio 12
+% Caso base: se o segundo número for 0, o resultado é o primeiro
+mdc(A, 0, A) :- A >= 0.
+
+% Caso recursivo: mdc(A, B) = mdc(B, A mod B)
+mdc(A, B, R) :-
+    B > 0,
+    Rem is A mod B,
+    mdc(B, Rem, R).
+
+% Exercicio 13
+% Caso base: lista vazia -> 0 ocorrências
+ocorrencias(_, [], 0).
+
+% Caso recursivo: cabeça da lista é o elemento buscado
+ocorrencias(Elem, [Elem|Resto], N) :-
+    ocorrencias(Elem, Resto, N1),
+    N is N1 + 1.
+
+% Caso recursivo: cabeça diferente do elemento buscado
+ocorrencias(Elem, [_|Resto], N) :-
+    ocorrencias(Elem, Resto, N).
+
+
+% Exercicio 14
+% Gera todos os divisores de N
+divisores(N, Divisores) :-
+    N > 0,
+    findall(D, (between(1, N, D), N mod D =:= 0), Divisores).
+
+
+%EXERCICIOS EXTRAS
+
+ultimo([A], A).
+ultimo([_|B], X) :- ultimo(B, X).
+
+tamanho([], 0).
+tamanho([_|B], R) :- tamanho(B, R1), R is R1 + 1.
+
+pertence(X, [X|_]).
+pertence(X,[_|Q]) :- pertence(X, Q).
+
+soma([], 0).
+soma([A|B], R) :- soma(B, R1), R IS R1 + A.
+
+concatena([], L, L).
+concatena([A|B], L, [A|R]) :- concatena(B, L, R).
+
+inverter([], []).
+inverter([A|B], L) :- inverter (B, R), concatena(R, [A], L).
+
+mesmo_tamanho([], []).
+mesmo_tamanho([_|A], [_|B]) :- mesmo_tamanho(A, B).
+
+% Remover elementos consecutivos remove_duplicados
+
+remove_duplicados([], []).
+remove_duplicados([X], [X]).
+remove_duplicados([X, X|Resto], Resultado) :- remove_duplicados([X|Resto], Resultado).
+remove_duplicados([X, Y|Resto], [X|Resultado]) :- X \= Y, remove_duplicados([Y|Resto], Resultado).
+
+
+% imprime o penultimo elemento de uma lista
+
+penultimo(X, [X, _]).
+penultimo(X, [_|T]) :- penultimo(X, T).
