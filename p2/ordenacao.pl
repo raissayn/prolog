055df010841f0464cae87f55c02aa9f3265c446e
Raissa Nunes <raissan084@gmail.com>
2025-05-06 19:24:03 -0300

    att

--- p2/ordenacao.pl
+++ p2/ordenacao.pl
@@ -3,3 +3,18 @@
 insereord(X,[Y|T],(X,Y|T)) :- X =< Y, !.
 insereord(X,[Y|L],[Y|Lo]) :- insereord(X,L,Lo).
 insereord(X,[],[X]). 
+
+ordenaInsere([A|B], L) :- ordenaInsere(B, Li), insereird(A.Li,L).
+ordenaInsere([],[]).
+
+apaga(X,[X|Y],Y) :- !.
+apaga(X,[Y|Z],[Y|Zs]) :- apaga(X,Z,Zs).
+
+min(X,[X]) :- !.
+min(X,[X|Y]) :- min(N,Y), X < N, !.
+min(X,[_|Y]) :- min(X,Y).
+
+removeMin(M,L,Lo) :- min(M,L), apaga(M,L,Lo).
+
+ordenaSelecao(L,[M|S]) :- removeMin(M,L,Lo), ordenaSelecao(Lo,S).
+ordenaSelecao([],[]).
